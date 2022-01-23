83e7326cb8d5ffae7e8f1d5b0331a48795bfd560
stiem <98281535+stiem0@users.noreply.github.com>
2022-01-23 20:06:43 +0100

    Add files via upload

---
+++ help/code/bullet.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var run = 0
+var distance = 20
+
+
+
+
+
+func _physics_process(delta):
+	self.position+= Vector2(0.8,0).rotated(rotation)
+	if ($RayCast2D.is_colliding()):
+		print("true")
+	if run > 1000 or ($RayCast2D.is_colliding()) : 
+		 self.queue_free()
+	else :
+		run +=1
+	
+
